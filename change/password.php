08f49bae5945521b28b2a7da24f1bbbac24a413a
Federico Tensi <federicotensi@gmail.com>
2018-09-01 17:26:56 +0200

    change pw implemented

--- change/password.php
+++ change/password.php
@@ -1,6 +1,6 @@
 <?php
     // Initialize the session
-
+    require_once '../config.php';
     session_start();
 
 
@@ -12,13 +12,13 @@
 
     /*declare some varible*/
     $userdir ='./store/' . $_SESSION['username']; //used to know where to store file
-    $name = $_SESSION['username']; //used to the message "Welcome $name"
+    $username = $_SESSION['username']; //used to the message "Welcome $name"
 
 
 
      // Define variables and initialize with empty values
-     $curpassword = $newpassword = $confnewpassword ="";
-     $curpassword_err = $newpassword_err = $confnewpassword_err = "";
+     $curpassword = $newpassword = $confnewpassword = "";
+     $curpassword_err = $newpassword_err = $confnewpassword_err = $login_err ="";
 
    // Processing form data when form is submitted
    if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -32,24 +32,116 @@
         
     }
 
-    /*   
+    
     // Check if password is empty
     if(empty(trim($_POST['newpw']))){
         $newpassword_err = 'Please enter your password.';
     } else{
-        $newpassword = trim($_POST['newpw']);
+        $newpassword = $_POST['newpw'];
     }
 
   // Check if password is empty
     if(empty(trim($_POST['confnewpw']))){
         $confnetpassword_err = 'Please enter your password.';
     } else{
-        $confnewpassword = trim($_POST['confnewpw']);
-    }*/
+        $confnewpassword = $_POST['confnewpw'];
+    }
        }
 
+
+        //confronto le password
+        if($newpassword != $confnewpassword){
+            $confnewpassword_err = "Le nuove password non corrispondono!";
+        }else{
+            $confnewpassword_err = "Le nuove password corrispondono!";
+
+            //vedo se la currpw è vera
+            // Validate credentials
+        if(empty($login_err) && empty($login_err)){
+            // Prepare a select statement
+            $sql = "SELECT username, password FROM users WHERE username = ?";
+
+            if($stmt = $mysqli->prepare($sql)){
+                // Bind variables to the prepared statement as parameters
+                $stmt->bind_param("s", $param_username);
+
+                // Set parameters
+                $param_username = $username;
+
+
+
+                
+                
+
+                // Attempt to execute the prepared statement
+                if($stmt->execute()){
+                    // Store result
+                    $stmt->store_result();
+
+                    // Check if username exists, if yes then verify password
+                    if($stmt->num_rows == 1){
+                        // Bind result variables
+                        $stmt->bind_result($username, $hashed_password);
+                        if($stmt->fetch()){
+                            if(password_verify($curpassword, $hashed_password)){
+                                /* Password is correct, so start a new session and
+                                save the username to the session */
+                                
+                                $hashed_newpassword = password_hash($newpassword, PASSWORD_DEFAULT); // Creates a password hash
+                                $login_err = "all ok";
+                               $query = "UPDATE users SET password = '$hashed_newpassword' WHERE username = '$username';";
+                               
+                               /*INSERT INTO users (password) SELECT username FROM users WHERE username = ($username);*/
+                                
+
+                                if(mysqli_query($mysqli, $query)) {
+                                   
+                                    echo "Successfully inserted " . mysqli_affected_rows($mysqli) . " row";
+                                   
+                                    } else {
+                                  
+                                    echo "Error occurred: " . mysqli_error($mysqli);
+                                 
+                                    }
+                               
+                               
+                            } else{
+                                // Display an error message if password is not valid
+                                $login_err = 'The password you entered was not valid.';
+                            }
+                        }
+                    } else{
+                        // Display an error message if username doesn't exist
+                        $login_err = 'No account found with that username.';
+                    }
+                } else{
+                    echo "Oops! Something went wrong. Please try again later.";
+                }
+            }
+
+            // Close statement
+            $stmt->close();
+        }
+
+        // Close connection
+        $mysqli->close();
+
+
+    }
+       echo "Errore:";
+       print ($login_err);
+       echo "<br>"; 
+       echo "Current user:";
+       print ($username);
+       echo "<br>";
        echo "curpassword:";
        print ($curpassword);
+       echo "<br>";
+       echo "newpassword:";
+       print ($newpassword);
+       echo "<br>";
+       echo "confnewpassword:";
+       print ($confnewpassword);
 ?>
 
     <!DOCTYPE html>
@@ -67,20 +159,20 @@
 
 
 <div class="container">
-<form >
+<form method="post">
   <div class="form-group">
-    <label for="curpw">Current Password</label>
-    <input type="text" name="currpw" class="form-control"value="<?php echo $curpassword; ?>"  placeholder="Current Password">
+    <label for="currpw">Current Password</label>
+    <input type="text" name="currpw" class="form-control" value="<?php echo $curpassword; ?>"  placeholder="Current Password">
     <span class="help-block rosso"><?php echo $curpassword_err; ?></span>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">New Password</label>
-    <input type="confirm_password" class="form-control" id="newpw"  placeholder="New Password">
+    <input type="password" class="form-control" name="newpw" value="<?php echo $newpassword; ?>" placeholder="New Password">
     <span class="help-block rosso"><?php echo $newpassword_err; ?></span>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Confirm New Password</label>
-    <input type="confirm_password2" class="form-control" id="confnewpw"  placeholder="Confirm New Password">
+    <input type="password" class="form-control" name="confnewpw" value="<?php echo $confnewpassword; ?>" placeholder="Confirm New Password">
     <span class="help-block rosso"><?php echo $confnewpassword_err; ?></span>
   </div>
 
@@ -90,4 +182,4 @@
 
     <?php include "../include/foo.php" ?>   
     </body>
-    </html>
+ </html>
